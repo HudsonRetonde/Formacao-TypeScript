d76e5ffc4f26eaebf2c0bca625c4c16cb39aa7e2
Hudson Retonde <hudsonsretonde@gmail.com>
2022-08-17 21:35:44 -0300

    Subindo o projeto.

---
+++ app/views/view.ts
@@ -0,0 +1,29 @@
+export abstract class View<T> {
+    protected elemento: HTMLElement;
+    private escapar = false;
+
+    constructor(seletor: string, escapar?: boolean) {
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no Dom. Verifique!`)
+        }
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+    
+    public update(model: T): void {
+        const t1 = performance.now();
+        let template = this.template(model);
+        if(this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.elemento.innerHTML = template;
+        const t2 = performance.now();
+        console.log(`Tempo de execução do método update: ${t2 - t1} segundos.`);
+    }
+
+    protected abstract template(model: T): string;
+}
