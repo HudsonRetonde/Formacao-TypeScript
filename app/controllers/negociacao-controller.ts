a1d6f46d40bca77709809b4f2b393685e7a00aba
Hudson Retonde <hudsonsretonde@gmail.com>
2022-08-20 22:48:41 -0300

    Adcionando Camadas de Serviço.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { NegociacoesDoDia } from './../src/interfaces/negociacao-do-dia.js';
 import { DiasDaSemana } from './../enums/dias-da-semana.js';
 import { MensagemView } from './../views/mensagem-view.js';
 import { Negociacao } from '../models/negociacao.js';
@@ -45,19 +46,13 @@ export class NegociacaoController {
     }
 
     public importaDados(): void {
-        fetch('http://localhost:8080/dados')
-            .then(res => res.json())
-            .then((dados: any[]) =>{
-                dados.map(dadosDeHoje =>{
-                    return new Negociacao(new Date(), dadosDeHoje.vezes, dadosDeHoje.montante)
-                })
-            })
-            .then((negociacoesDeHoje: any) => {
+       /*
+            .then((negociacoesDeHoje) => {
                 for(let negociacao of negociacoesDeHoje) {
                     this.negociacoes.adiciona(negociacao);
                 }
                 this.negociacoesView.update(this.negociacoes);
-            })
+            }) */
     }
 
     private ehDiaUtil(data: Date){
